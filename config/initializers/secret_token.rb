1e133c3a18f981ff84bf73248128a7d5eb6715b5
James <james@finedesigngroup.com>
2012-05-24 10:06:20 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pdfgenerator::Application.config.secret_token = '134e7d22f20fce9d3c2c286f5f58cc3b58ddb2aeb392ce9385ba112abce4b3dc272ff8953466d68876490de87af1a46f89fc5e8754bdeec3bb571658c139978f'
